ae048e98b0bbc7c6f8062b1bc3f4ae22f263935f
Brian Lucca <brianlucca2002@gmail.com>
2026-08-20 21:51:51 -0300

    fix(config): update next-config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Diz ao Next.js para não tentar compilar o firebase-admin
   serverExternalPackages: ["firebase-admin"],
-
-  typescript: {
-    ignoreBuildErrors: true,
+  async headers() {
+    return [{
+      source: "/(.*)",
+      headers: [
+        { key: "Content-Security-Policy", value: "base-uri 'self'; frame-ancestors 'none'; object-src 'none'" },
+        { key: "X-Content-Type-Options", value: "nosniff" },
+        { key: "X-Frame-Options", value: "DENY" },
+        { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+        { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=()" },
+      ],
+    }];
   },
 };
 
