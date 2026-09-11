c0c78466b1353a15c80c33ba04946ab22e312dd4
Brian Lucca <brianlucca2002@gmail.com>
2026-09-10 21:01:51 -0300

    style: refresh shared input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,7 +1,7 @@
-import * as React from "react"
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import { cn } from "@/lib/utils";
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -9,15 +9,15 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-11 w-full rounded-lg border border-white/10 bg-[#0B0E14] px-3 py-2 text-sm text-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500/50 disabled:cursor-not-allowed disabled:opacity-50",
-          className
+          "flex h-11 w-full rounded-xl border border-[#dedce1] bg-white px-3 py-2 text-sm text-[#292a30] file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-[#a0a2a9] focus-visible:border-[#8c86ec] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#635bff]/15 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
         )}
         ref={ref}
         {...props}
       />
-    )
-  }
-)
-Input.displayName = "Input"
+    );
+  },
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
